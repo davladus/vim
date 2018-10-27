bc2aaaa2cde03aef01c435bb71ca343df1a2c330
Afinageny <afinageny@gmail.com>
2018-10-27 11:20:07 +0300

    save

---
+++ config-vim-old/vimfiles/bundle/get.bat
@@ -0,0 +1,13 @@
+
+rem git clone https://github.com/vim-scripts/paredit.vim.git
+git clone https://github.com/kien/ctrlp.vim.git
+git clone git://github.com/kien/rainbow_parentheses.vim.git
+git clone git://github.com/jceb/vim-orgmode.git
+git clone git://github.com/tpope/vim-fireplace.git
+git clone git://github.com/tpope/vim-classpath.git
+git clone git://github.com/guns/vim-clojure-static.git
+
+
+
+
+
